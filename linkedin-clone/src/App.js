0c2edddd3085ca4b24bf2514c321530f87dde383
waterkey15 <hdorukkilic@hotmail.com>
2021-12-16 14:31:26 -0500

    login frontend, header frontend done

---
+++ linkedin-clone/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import Login from './components/Login';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Header from './components/Header';
+import Home from './components/Home';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Login/>
+          </Route>
+          <Route exact path='/home'>
+            <Header/>
+            <Home/>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
